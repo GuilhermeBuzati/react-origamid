fc3bb817a7e1b4d6d889700f9431867ac3a673fb
Guilherme Buzati <guilhermeleopoldinoo@gmail.com>
2024-02-24 20:20:37 -0300

    feat: 0103 JavaScript 3

---
+++ 0103-javascript-3/script.js
@@ -0,0 +1,15 @@
+const grupoA = 100;
+const grupoB = 300;
+const vencedor = grupoA > grupoB ? 'Grupo A Venceu' : 'Grupo B Venceu';
+
+console.log(vencedor)
+
+const numeros = [2, 3, 4, 5, 6];
+const total = numeros.filter((numero) => numero > 4);
+
+console.log(total)
+
+const active = true;
+const button = active && 'Botão está ativo';
+
+console.log(button)
